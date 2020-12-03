3c334d9cfdc6e67f749a0cfce67a28acfb98cda1
AliceLR <petrifiedrowan@gmail.com>
2020-12-03 04:11:31 -0700

    Fix mdl_load.c out-of-bounds read caused by invalid track packing.

---
+++ test-dev/test_fuzzer_mdl_invalid_run.c
@@ -0,0 +1,19 @@
+#include "test.h"
+
+/* This input caused out-of-bound reads in the MDL loader
+ * due to using a run event code as the first event in a track.
+ */
+
+TEST(test_fuzzer_mdl_invalid_run)
+{
+	xmp_context opaque;
+	struct xmp_module_info info;
+	int ret;
+
+	opaque = xmp_create_context();
+	ret = xmp_load_module(opaque, "data/f/load_mdl_invalid_run.mdl");
+	fail_unless(ret == -XMP_ERROR_LOAD, "module load");
+
+	xmp_free_context(opaque);
+}
+END_TEST
