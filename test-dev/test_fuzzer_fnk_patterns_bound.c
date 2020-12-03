e275fe5cb88152e08a0db00cf7e08cd791402d9f
AliceLR <petrifiedrowan@gmail.com>
2020-12-03 02:27:57 -0700

    Add test cases for fuzzing crashes.

---
+++ test-dev/test_fuzzer_fnk_patterns_bound.c
@@ -0,0 +1,19 @@
+#include "test.h"
+
+/* This input crashed the FNK loader due to the FNK maximum pattern
+ * count of 128 (due to the break position array) not being enforced.
+ */
+
+TEST(test_fuzzer_fnk_patterns_bound)
+{
+	xmp_context opaque;
+	struct xmp_module_info info;
+	int ret;
+
+	opaque = xmp_create_context();
+	ret = xmp_load_module(opaque, "data/f/load_fnk_patterns_bound.fnk");
+	fail_unless(ret == -XMP_ERROR_LOAD, "module load");
+
+	xmp_free_context(opaque);
+}
+END_TEST
---
+++ test-dev/test_fuzzer_psm_samples_bound.c
@@ -0,0 +1,19 @@
+#include "test.h"
+
+/* This input crashed the PSM loader due to containing a samples
+ * count over 64, which overflowed the sample offsets array.
+ */
+
+TEST(test_fuzzer_psm_samples_bound)
+{
+	xmp_context opaque;
+	struct xmp_module_info info;
+	int ret;
+
+	opaque = xmp_create_context();
+	ret = xmp_load_module(opaque, "data/f/load_psm_samples_bound.psm");
+	fail_unless(ret == -XMP_ERROR_LOAD, "module load");
+
+	xmp_free_context(opaque);
+}
+END_TEST
