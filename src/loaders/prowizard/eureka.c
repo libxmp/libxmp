42513235891cfbd8cc90f192885c8baaa7d002c8
AliceLR <petrifiedrowan@gmail.com>
2020-12-09 03:35:28 -0700

    Fix Eureka segmentation faults during format check.

--- src/loaders/prowizard/eureka.c
+++ src/loaders/prowizard/eureka.c
@@ -163,6 +163,8 @@ static int test_eu(const uint8 *data, char *t, int s)
 			min_trk = trk;
 	}
 
+	PW_REQUEST_DATA(s, max_trk + (max_pat * 4 * 2) + 1085);
+
 	/* test track datas */
 	/* last track wont be tested ... */
 	for (i = min_trk; i < max_trk; i++) {
