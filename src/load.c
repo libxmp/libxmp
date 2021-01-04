0f8731a061b1e4fea80386f75606a49ca0d61de6
Friendly <Hacker>
2021-01-04 09:23:21 -0500

    Again

--- src/load.c
+++ src/load.c
@@ -692,8 +692,7 @@ void xmp_release_module(xmp_context opaque)
 		return;
 	 */
 
-	ctx->state = XMP_STATE_PLAYING;
-	if (ctx->state > XMP_STATE_LOADED)
+	//if (ctx->state > XMP_STATE_LOADED)
 		xmp_end_player(opaque);
 
 	ctx->state = XMP_STATE_UNLOADED;
