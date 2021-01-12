9203087f4c921af63a0f941f7c007c1218c9b1a0
AliceLR <petrifiedrowan@gmail.com>
2021-01-12 15:15:30 -0700

    Skip loading samples that exist at or after EOF.

---
+++ test-dev/test_fuzzer_s3m_invalid_sample_size.c
@@ -0,0 +1,19 @@
+#include "test.h"
+
+/* This input caused hangs and excessive memory consumption in the S3M
+ * loader due to invalid sample sizes.
+ */
+
+TEST(test_fuzzer_s3m_invalid_sample_size)
+{
+	xmp_context opaque;
+	struct xmp_module_info info;
+	int ret;
+
+	opaque = xmp_create_context();
+	ret = xmp_load_module(opaque, "data/f/load_s3m_invalid_sample_size.s3m");
+	fail_unless(ret == -XMP_ERROR_LOAD, "module load");
+
+	xmp_free_context(opaque);
+}
+END_TEST
