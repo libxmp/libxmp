df0da98f443669ba794208ce9a5a7fea620adeaf
AliceLR <petrifiedrowan@gmail.com>
2021-03-05 20:23:14 -0700

    Fix STMIK instrument name UMRs caused by missing EOF checks.

--- test-dev/test_fuzzer_stx_truncated.c
+++ test-dev/test_fuzzer_stx_truncated.c
@@ -1,7 +1,7 @@
 #include "test.h"
 
-/* This input caused hangs due to a missing EOF check in the
- * STMIK pattern loading loop.
+/* These inputs caused hangs and UMRs due to a missing EOF checks
+ * in the STMIK loader.
  */
 
 TEST(test_fuzzer_stx_truncated)
@@ -11,7 +11,12 @@ TEST(test_fuzzer_stx_truncated)
 	int ret;
 
 	opaque = xmp_create_context();
+	/* Truncated pattern (hang). */
 	ret = xmp_load_module(opaque, "data/f/load_stx_truncated.stx");
+	fail_unless(ret == -XMP_ERROR_LOAD, "module load");
+
+	/* Truncated instrument name (UMR). */
+	ret = xmp_load_module(opaque, "data/f/load_stx_truncated2.stx");
 	fail_unless(ret == -XMP_ERROR_LOAD, "module load");
 
 	xmp_free_context(opaque);
