fa066c76cb5b7ee59d2d145f54f38155bc5720d6
Ozkan Sezer <sezeroz@gmail.com>
2021-03-27 01:40:04 +0300

    use libxmp replacement of round() for djgpp.

--- src/period.c
+++ src/period.c
@@ -137,7 +137,7 @@ static uint16 pt_period_table[16][36] = {
 #ifndef M_LN2
 #define M_LN2	0.69314718055994530942
 #endif
-#if !defined(HAVE_ROUND) || defined(_MSC_VER) || defined(__WATCOMC__)
+#if !defined(HAVE_ROUND) || defined(_MSC_VER) || defined(__WATCOMC__) || defined(__DJGPP__)
 static inline double libxmp_round(double val)
 {
 	return (val >= 0.0)? floor(val + 0.5) : ceil(val - 0.5);
