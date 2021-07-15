b1bed43c9f5e03c6be057e0e82ed8229984d2f39
AliceLR <petrifiedrowan@gmail.com>
2021-07-15 16:40:39 -0600

    Fix double free in stb-vorbis.

--- test-dev/test_fuzzer_xm_vorbis_crash.c
+++ test-dev/test_fuzzer_xm_vorbis_crash.c
@@ -1,6 +1,6 @@
 #include "test.h"
 
-/* This input OXM caused NULL dereferences in stb-vorbis.
+/* Inputs that caused crashes in libxmp's Ogg Vorbis decoder (stb-vorbis).
  */
 
 TEST(test_fuzzer_xm_vorbis_crash)
@@ -10,8 +10,16 @@ TEST(test_fuzzer_xm_vorbis_crash)
 	int ret;
 
 	opaque = xmp_create_context();
+
+	/* This input OXM caused NULL dereferences in stb-vorbis.
+	 */
 	ret = xmp_load_module(opaque, "data/f/load_xm_vorbis_crash.oxm");
-	fail_unless(ret == -XMP_ERROR_DEPACK, "module load");
+	fail_unless(ret == -XMP_ERROR_DEPACK, "module load (1)");
+
+	/* This input OXM caused double frees in stb-vorbis due to
+	 * a libxmp fix conflicting with upstream. */
+	ret = xmp_load_module(opaque, "data/f/load_xm_vorbis_crash2.oxm");
+	fail_unless(ret == -XMP_ERROR_DEPACK, "module load (2)");
 
 	xmp_free_context(opaque);
 }
