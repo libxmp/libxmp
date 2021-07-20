adf8512b0a6ec6e533e46210dc95a84fb296952c
Ozkan Sezer <sezeroz@gmail.com>
2021-07-20 10:10:02 +0300

    remove two other left-over -Isrc directives.

--- lite/jni/Android.mk
+++ lite/jni/Android.mk
@@ -10,7 +10,7 @@ LOADERS_SOURCES := $(addprefix src/loaders/,$(LOADERS_OBJS))
 
 LOCAL_MODULE    := xmp
 LOCAL_CFLAGS	:= -O3 -DHAVE_MKSTEMP -DHAVE_FNMATCH -DHAVE_POWF -DLIBXMP_CORE_PLAYER \
-		   -I$(LOCAL_PATH)/include/libxmp-lite -I$(LOCAL_PATH)/src
+		   -I$(LOCAL_PATH)/include/libxmp-lite
 LOCAL_SRC_FILES := $(SRC_SOURCES:.o=.c.arm) \
 		   $(LOADERS_SOURCES:.o=.c)
 
