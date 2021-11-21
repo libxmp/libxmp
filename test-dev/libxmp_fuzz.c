4a0dc7649f44ddbea69655d9b528c2933cee083f
AliceLR <petrifiedrowan@gmail.com>
2021-11-20 18:19:43 -0700

    Add simple playback fuzzing and compare_playback for testing.

--- test-dev/libxmp_fuzz.c
+++ test-dev/libxmp_fuzz.c
@@ -14,7 +14,33 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 	/* Fuzz loaders. */
 	if (xmp_load_module_from_memory(opaque, data, size) == 0)
 	{
-		/* FIXME fuzz playback. */
+		/* Fuzz playback. */
+		struct xmp_module_info info;
+		int interp, mono, i;
+
+		/* Derive config from the MD5 for now... :( */
+		xmp_get_module_info(opaque, &info);
+		interp = info.md5[7] * 3U / 256;
+		mono = (info.md5[3] & 1) ^ (info.md5[14] >> 7);
+
+		switch (interp) {
+		case 0:
+			interp = XMP_INTERP_NEAREST;
+			break;
+		case 1:
+			interp = XMP_INTERP_LINEAR;
+			break;
+		default:
+			interp = XMP_INTERP_SPLINE;
+			break;
+		}
+		xmp_start_player(opaque, XMP_MIN_SRATE, mono ? XMP_FORMAT_MONO : 0);
+		xmp_set_player(opaque, XMP_PLAYER_INTERP, interp);
+
+		/* TODO Saga Musix also recommends performing different types of seeking. */
+		for (i = 0; i < 64; i++)
+			xmp_play_frame(opaque);
+
 		xmp_release_module(opaque);
 	}
 
