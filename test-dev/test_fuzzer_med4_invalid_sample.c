f74b9e3bc8fc42249a71256ea37f6a31cd3984ac
AliceLR <petrifiedrowan@gmail.com>
2021-11-20 22:10:12 -0700

    Fix signed integer overflow with MED4 synth lengths.

--- test-dev/test_fuzzer_med4_invalid_sample.c
+++ test-dev/test_fuzzer_med4_invalid_sample.c
@@ -11,10 +11,13 @@ TEST(test_fuzzer_med4_invalid_sample)
 	opaque = xmp_create_context();
 
 	ret = xmp_load_module(opaque, "data/f/load_med4_invalid_sample.med");
-	fail_unless(ret == 0, "module load");
+	fail_unless(ret == 0, "module load (1)");
 
 	ret = xmp_load_module(opaque, "data/f/load_med4_invalid_sample2.med");
-	fail_unless(ret == -XMP_ERROR_LOAD, "module load");
+	fail_unless(ret == -XMP_ERROR_LOAD, "module load (2)");
+
+	ret = xmp_load_module(opaque, "data/f/load_med4_invalid_sample3.med");
+	fail_unless(ret == -XMP_ERROR_LOAD, "module load (3)");
 
 	xmp_free_context(opaque);
 }
