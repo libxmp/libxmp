723851d844c84e4781590f1ff8bbfbb8a4d99432
Ozkan Sezer <sezeroz@gmail.com>
2022-02-10 14:50:56 +0300

    bunzip2.c: added missing license notice.

--- src/depackers/bunzip2.c
+++ src/depackers/bunzip2.c
@@ -8,7 +8,18 @@
  * Alistair Moffat, Radford Neal, Ian H. Witten, Robert Sedgewick, and
  * Jon L. Bentley.
  *
- * No standard.
+ * This is 0BSD-licensed code from https://github.com/landley/toybox:
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include <setjmp.h>
