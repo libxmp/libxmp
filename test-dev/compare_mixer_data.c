a64ba69cf37005ec5ab607e331b207491b87aafd
AliceLR <petrifiedrowan@gmail.com>
2022-02-10 02:31:38 -0700

    Test NNA channels, filter resonance in compare_mixer_data.

--- test-dev/compare_mixer_data.c
+++ test-dev/compare_mixer_data.c
@@ -8,14 +8,14 @@ static void _compare_mixer_data(const char *mod, const char *data, int loops, in
 {
 	xmp_context opaque;
 	struct context_data *ctx;
-	struct module_data *m;
 	struct player_data *p;
 	struct mixer_voice *vi;
 	struct xmp_frame_info fi;
-	int time, row, frame, chan, period, note, ins, vol, pan, pos0, cutoff;
+	int time, row, frame, chan, period, note, ins, vol, pan, pos0, cutoff, resonance;
 	char line[200];
 	FILE *f;
 	int i, voc, ret;
+	int max_channels;
 
 	f = fopen(data, "r");
 	fail_unless(f != NULL, "can't open data file");
@@ -27,11 +27,12 @@ static void _compare_mixer_data(const char *mod, const char *data, int loops, in
 	fail_unless(ret == 0, "can't load module");
 
 	ctx = (struct context_data *)opaque;
-	m = &ctx->m;
 	p = &ctx->p;
 
 	xmp_start_player(opaque, 44100, 0);
 	xmp_set_player(opaque, XMP_PLAYER_MIX, 100);
+
+	max_channels = p->virt.virt_channels;
 
 	while (1) {
 		xmp_play_frame(opaque);
@@ -39,9 +40,12 @@ static void _compare_mixer_data(const char *mod, const char *data, int loops, in
 		if (fi.loop_count >= loops)
 			break;
 
-		for (i = 0; i < m->mod.chn; i++) {
-			struct xmp_channel_info *ci = &fi.channel_info[i];
+		for (i = 0; i < max_channels; i++) {
+			/* Channel info doesn't get updated for NNA channels,
+			 * need to get the info period from the channel data */
+			/*struct xmp_channel_info *ci = &fi.channel_info[i];*/
 			struct channel_data *xc = &p->xc_data[i];
+			int ci_period = xc->info_period;
 			int num;
 
 			voc = map_channel(p, i);
@@ -52,16 +56,16 @@ static void _compare_mixer_data(const char *mod, const char *data, int loops, in
 
 #if 1
 			fail_unless(fgets(line, 200, f) != NULL, "early EOF");
-			num = sscanf(line, "%d %d %d %d %d %d %d %d %d %d %d",
+			num = sscanf(line, "%d %d %d %d %d %d %d %d %d %d %d %d",
 				&time, &row, &frame, &chan, &period,
-				&note, &ins, &vol, &pan, &pos0, &cutoff);
+				&note, &ins, &vol, &pan, &pos0, &cutoff, &resonance);
 
 			/* Allow some error in values derived from floating point math. */
 			fail_unless(abs(fi.time - time) <= 1, "time mismatch");
 			fail_unless(fi.row     == row,    "row mismatch");
 			fail_unless(fi.frame   == frame,  "frame mismatch");
 			fail_unless(i          == chan,   "channel mismatch");
-			fail_unless(ci->period == period, "period mismatch");
+			fail_unless(ci_period  == period, "period mismatch");
 			fail_unless(vi->note   == note,   "note mismatch");
 			fail_unless(vi->ins    == ins,    "instrument");
 			if (!ignore_rv) {
@@ -79,10 +83,15 @@ static void _compare_mixer_data(const char *mod, const char *data, int loops, in
 				fail_unless(vi->filter.cutoff == cutoff,
 							  "cutoff mismatch");
 			}
+			if (num >= 12) {
+				fail_unless(vi->filter.resonance == resonance,
+					    "resonance mismatch");
+			}
 #else
-			fprintf(f, "%d %d %d %d %d %d %d %d %d %d %d\n",
-				fi.time, fi.row, fi.frame, i, ci->period,
-				vi->note, vi->ins, vi->vol, vi->pan, vi->pos0, vi->filter.cutoff);
+			fprintf(f, "%d %d %d %d %d %d %d %d %d %d %d %d\n",
+				fi.time, fi.row, fi.frame, i, xc->info_period,
+				vi->note, vi->ins, vi->vol, vi->pan, vi->pos0,
+				vi->filter.cutoff, vi->filter.resonance);
 #endif
 		}
 	}
