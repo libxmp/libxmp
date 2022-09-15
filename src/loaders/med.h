fa336917778e98ae246454cfba39f75e6a99b649
Ozkan Sezer <sezeroz@gmail.com>
2022-09-15 23:37:56 +0300

    remove amiga/windows types use in med.h and rtm_load.c.
    Closes https://github.com/libxmp/libxmp/issues/614

--- src/loaders/med.h
+++ src/loaders/med.h
@@ -10,45 +10,34 @@
 extern const char *const mmd_inst_type[];
 #endif
 
-typedef int32 LONG;
-typedef uint32 ULONG;
-typedef int16 WORD;
-typedef uint16 UWORD;
-typedef int8 BYTE;
-typedef uint8 UBYTE;
-#ifndef __amigaos4__
-typedef char *STRPTR;
-#endif
-
-
 /* Structures as defined in the MED/OctaMED MMD0 and MMD1 file formats,
  * revision 1, described by Teijo Kinnunen in Apr 25 1992
  */
 
-struct PlaySeq {                                                            
+struct PlaySeq {
     char name[32];		/* (0)  31 chars + \0 */
-    ULONG reserved[2];		/* (32) for possible extensions */
-    UWORD length;		/* (40) # of entries */
-    UWORD seq[1];		/* (42) block numbers.. */
-};                                                      
+    uint32 reserved[2];		/* (32) for possible extensions */
+    uint16 length;		/* (40) # of entries */
+    uint16 seq[1];		/* (42) block numbers.. */
+};
 
 
 struct MMD0sample {
-    UWORD rep, replen;		/* offs: 0(s), 2(s) */
-    UBYTE midich;		/* offs: 4(s) */
-    UBYTE midipreset;		/* offs: 5(s) */
-    UBYTE svol;			/* offs: 6(s) */
-    BYTE strans;		/* offs: 7(s) */
-}; 
+    uint16 rep, replen;		/* offs: 0(s), 2(s) */
+    uint8 midich;		/* offs: 4(s) */
+    uint8 midipreset;		/* offs: 5(s) */
+    uint8 svol;			/* offs: 6(s) */
+    int8 strans;		/* offs: 7(s) */
+};
 
 
 struct MMD0song {
     struct MMD0sample sample[63];	/* 63 * 8 bytes = 504 bytes */
-    UWORD numblocks;		/* offs: 504 */
-    UWORD songlen;		/* offs: 506 */
-    UBYTE playseq[256];		/* offs: 508 */
-    UWORD deftempo;		/* offs: 764 */
-    BYTE playtransp;		/* offs: 766 */
+    uint16 numblocks;		/* offs: 504 */
+    uint16 songlen;		/* offs: 506 */
+    uint8 playseq[256];		/* offs: 508 */
+    uint16 deftempo;		/* offs: 764 */
+    int8 playtransp;		/* offs: 766 */
 #define FLAG_FILTERON   0x1	/* the hardware audio filter is on */
 #define FLAG_JUMPINGON  0x2	/* mouse pointer jumping on */
 #define FLAG_JUMP8TH    0x4	/* ump every 8th line (not in OctaMED Pro) */
@@ -57,15 +46,15 @@ struct MMD0song {
 #define FLAG_STSLIDE    0x20	/* use ST/NT/PT compatible sliding */
 #define FLAG_8CHANNEL   0x40	/* this is OctaMED 5-8 channel song */
 #define FLAG_SLOWHQ     0X80	/* HQ V2-4 compatibility mode */
-    UBYTE flags;		/* offs: 767 */
+    uint8 flags;		/* offs: 767 */
 #define FLAG2_BMASK	0x1F	/* (bits 0-4) BPM beat length (in lines) */
 #define FLAG2_BPM       0x20	/* BPM mode on */
 #define FLAG2_MIX	0x80	/* Module uses mixing */
-    UBYTE flags2;		/* offs: 768 */
-    UBYTE tempo2;		/* offs: 769 */
-    UBYTE trkvol[16];		/* offs: 770 */
-    UBYTE mastervol;		/* offs: 786 */
-    UBYTE numsamples;		/* offs: 787 */
+    uint8 flags2;		/* offs: 768 */
+    uint8 tempo2;		/* offs: 769 */
+    uint8 trkvol[16];		/* offs: 770 */
+    uint8 mastervol;		/* offs: 786 */
+    uint8 numsamples;		/* offs: 787 */
 };				/* length = 788 bytes */
 
 
@@ -75,241 +64,241 @@ struct MMD0song {
  */
 struct MMD2song {
     struct MMD0sample sample[63];
-    UWORD numblocks;
-    UWORD songlen;		/* NOTE: number of sections in MMD2 */
+    uint16 numblocks;
+    uint16 songlen;		/* NOTE: number of sections in MMD2 */
     struct PlaySeq **playseqtable;
-    UWORD *sectiontable;	/* UWORD section numbers */
-    UBYTE *trackvols;		/* UBYTE track volumes */
-    UWORD numtracks;		/* max. number of tracks in the song
+    uint16 *sectiontable;	/* UWORD section numbers */
+    uint8 *trackvols;		/* UBYTE track volumes */
+    uint16 numtracks;		/* max. number of tracks in the song
 				 * (also the number of entries in
 				 * 'trackvols' table) */
-    UWORD numpseqs;		/* number of PlaySeqs in 'playseqtable' */
-    BYTE *trackpans;		/* NULL means 'all centered */
+    uint16 numpseqs;		/* number of PlaySeqs in 'playseqtable' */
+    int8 *trackpans;		/* NULL means 'all centered */
 #define FLAG3_STEREO	0x1	/* Mixing in stereo */
 #define FLAG3_FREEPAN	0x2	/* Mixing flag: free pan */
-    ULONG flags3;		/* see defs below */
-    UWORD voladj;		/* volume adjust (%), 0 means 100 */
-    UWORD channels;		/* mixing channels, 0 means 4 */
-    UBYTE mix_echotype;		/* 0 = nothing, 1 = normal, 2 = cross */
-    UBYTE mix_echodepth;	/* 1 - 6, 0 = default */
-    UWORD mix_echolen;		/* echo length in milliseconds */
-    BYTE mix_stereosep;		/* stereo separation */
-    UBYTE pad0[223];		/* reserved for future expansion */
+    uint32 flags3;		/* see defs below */
+    uint16 voladj;		/* volume adjust (%), 0 means 100 */
+    uint16 channels;		/* mixing channels, 0 means 4 */
+    uint8 mix_echotype;		/* 0 = nothing, 1 = normal, 2 = cross */
+    uint8 mix_echodepth;	/* 1 - 6, 0 = default */
+    uint16 mix_echolen;		/* echo length in milliseconds */
+    int8 mix_stereosep;		/* stereo separation */
+    uint8 pad0[223];		/* reserved for future expansion */
 /* Fields below are MMD0/MMD1-compatible (except pad1[]) */
-    UWORD deftempo;
-    BYTE playtransp;
-    UBYTE flags;
-    UBYTE flags2;
-    UBYTE tempo2;
-    UBYTE pad1[16];		/* used to be trackvols, in MMD2 reserved */
-    UBYTE mastervol;
-    UBYTE numsamples;
+    uint16 deftempo;
+    int8 playtransp;
+    uint8 flags;
+    uint8 flags2;
+    uint8 tempo2;
+    uint8 pad1[16];		/* used to be trackvols, in MMD2 reserved */
+    uint8 mastervol;
+    uint8 numsamples;
 };				/* length = 788 bytes */
 
 
 struct MMD0 {
-    ULONG id;
-    ULONG modlen;
+    uint32 id;
+    uint32 modlen;
     struct MMD0song *song;
-    UWORD psecnum;			/* MMD2 only */
-    UWORD pseq;				/* MMD2 only */
+    uint16 psecnum;			/* MMD2 only */
+    uint16 pseq;			/* MMD2 only */
     struct MMD0Block **blockarr;
 #define MMD_LOADTOFASTMEM 0x1
-    UBYTE mmdflags;			/* MMD2 only */
-    UBYTE reserved[3];
+    uint8 mmdflags;			/* MMD2 only */
+    uint8 reserved[3];
     struct InstrHdr **smplarr;
-    ULONG reserved2;
+    uint32 reserved2;
     struct MMD0exp *expdata;
-    ULONG reserved3;
-    UWORD pstate;			/* some data for the player routine */
-    UWORD pblock;
-    UWORD pline;
-    UWORD pseqnum;
-    WORD actplayline;
-    UBYTE counter;
-    UBYTE extra_songs;			/* number of songs - 1 */
+    uint32 reserved3;
+    uint16 pstate;			/* some data for the player routine */
+    uint16 pblock;
+    uint16 pline;
+    uint16 pseqnum;
+    int16 actplayline;
+    uint8 counter;
+    uint8 extra_songs;			/* number of songs - 1 */
 };					/* length = 52 bytes */
 
 
-struct MMD0Block {                                                          
-    UBYTE numtracks, lines;                                                  
-};                     
+struct MMD0Block {
+    uint8 numtracks, lines;
+};
 
 
-struct BlockCmdPageTable {                                                      
-    UWORD num_pages;                                                      
-    UWORD reserved;                                                       
-    UWORD *page[1];                                                       
+struct BlockCmdPageTable {
+    uint16 num_pages;
+    uint16 reserved;
+    uint16 *page[1];
 };
 
 
 struct BlockInfo {
-    ULONG *hlmask;
-    UBYTE *blockname;
-    ULONG blocknamelen;
+    uint32 *hlmask;
+    uint8 *blockname;
+    uint32 blocknamelen;
     struct BlockCmdPageTable *pagetable;
-    ULONG reserved[5];
+    uint32 reserved[5];
 };
 
 
 struct MMD1Block {
-    UWORD numtracks;
-    UWORD lines;
+    uint16 numtracks;
+    uint16 lines;
     struct BlockInfo *info;
 };
 
 
 struct InstrHdr {
-    ULONG length;
+    uint32 length;
 #define S_16 0x10			/* 16-bit sample */
 #define MD16 0x18			/* 16-bit sample (Aura) */
 #define STEREO 0x20			/* Stereo sample, not interleaved */
-    WORD type;
+    int16 type;
     /* Followed by actual data */
 };
 
 
-struct SynthWF {                                                
-    UWORD length;			/* length in words */
-    BYTE  wfdata[1];			/* the waveform */
-};          
+struct SynthWF {
+    uint16 length;			/* length in words */
+    int8 wfdata[1];			/* the waveform */
+};
 
 
 struct SynthInstr {
-    ULONG length;			/* length of this struct */
-    WORD type;				/* -1 or -2 (offs: 4) */
-    UBYTE defaultdecay;
-    UBYTE reserved[3];
-    UWORD rep;
-    UWORD replen;
-    UWORD voltbllen;			/* offs: 14 */
-    UWORD wftbllen;			/* offs: 16 */
-    UBYTE volspeed;			/* offs: 18 */
-    UBYTE wfspeed;			/* offs: 19 */
-    UWORD wforms;			/* offs: 20 */
-    UBYTE voltbl[128];			/* offs: 22 */
-    UBYTE wftbl[128];			/* offs: 150 */
-    ULONG wf[64];			/* offs: 278 */
+    uint32 length;			/* length of this struct */
+    int16 type;				/* -1 or -2 (offs: 4) */
+    uint8 defaultdecay;
+    uint8 reserved[3];
+    uint16 rep;
+    uint16 replen;
+    uint16 voltbllen;			/* offs: 14 */
+    uint16 wftbllen;			/* offs: 16 */
+    uint8 volspeed;			/* offs: 18 */
+    uint8 wfspeed;			/* offs: 19 */
+    uint16 wforms;			/* offs: 20 */
+    uint8 voltbl[128];			/* offs: 22 */
+    uint8 wftbl[128];			/* offs: 150 */
+    uint32 wf[64];			/* offs: 278 */
 };
 
 
 struct InstrExt {
-    UBYTE hold;
-    UBYTE decay;
-    UBYTE suppress_midi_off;
-    BYTE  finetune;
+    uint8 hold;
+    uint8 decay;
+    uint8 suppress_midi_off;
+    int8  finetune;
     /* Below fields saved by >= V5 */
-    UBYTE default_pitch;
+    uint8 default_pitch;
 #define SSFLG_LOOP	0x01		/* Loop On/Off */
 #define SSFLG_EXTPSET	0x02		/* Ext. Preset */
 #define SSFLG_DISABLED	0x04		/* Disabled */
 #define SSFLG_PINGPONG	0x08		/* Ping-pong looping */
-    UBYTE instr_flags;
-    UWORD long_midi_preset;
+    uint8 instr_flags;
+    uint16 long_midi_preset;
     /* Below fields saved by >= V5.02 */
-    UBYTE output_device;
-    UBYTE reserved;
+    uint8 output_device;
+    uint8 reserved;
     /* Below fields saved by >= V7 */
-    ULONG long_repeat;
-    ULONG long_replen;
+    uint32 long_repeat;
+    uint32 long_replen;
 };
 
 
 struct MMDInfo {
     struct MMDInfo *next;		/* next MMDInfo structure */
-    UWORD reserved;
-    UWORD type;				/* data type (1 = ASCII) */
-    ULONG length;			/* data length in bytes */
-    /* data follows... */                                         
-};                                        
+    uint16 reserved;
+    uint16 type;				/* data type (1 = ASCII) */
+    uint32 length;			/* data length in bytes */
+    /* data follows... */
+};
 
 
-struct MMDARexxTrigCmd {                                      
+struct MMDARexxTrigCmd {
     struct MMDARexxTrigCmd *next;	/* the next command, or NULL */
-    UBYTE cmdnum;			/* command number (01..FF) */
-    UBYTE pad;
-    WORD cmdtype;			/* command type (OMACTION_...) */
-    STRPTR cmd;				/* command, or NULL */
-    STRPTR port;			/* port, or NULL */
-    UWORD cmd_len;			/* length of 'cmd' string (without
+    uint8 cmdnum;			/* command number (01..FF) */
+    uint8 pad;
+    int16 cmdtype;			/* command type (OMACTION_...) */
+    char *cmd;				/* command, or NULL */
+    char *port;				/* port, or NULL */
+    uint16 cmd_len;			/* length of 'cmd' string (without
 					 * term. 0) */
-    UWORD port_len;			/* length of 'port' string (without
+    uint16 port_len;			/* length of 'port' string (without
 					 * term. 0) */
-};		/* current (V7) structure size: 20 */             
+};		/* current (V7) structure size: 20 */
 
 
 struct MMDARexx {
-    UWORD res;				/* reserved, must be zero! */           
-    UWORD trigcmdlen;			/* size of trigcmd entries             
-					 * (MUST be used!!) */                 
+    uint16 res;				/* reserved, must be zero! */
+    uint16 trigcmdlen;			/* size of trigcmd entries
+					 * (MUST be used!!) */
     struct MMDARexxTrigCmd *trigcmd;	/* chain of MMDARexxTrigCmds or NULL */
-};                                               
+};
 
 
-struct MMDMIDICmd3x {                                         
-    UBYTE struct_vers;			/* current version = 0 */
-    UBYTE pad;                                                
-    UWORD num_of_settings;		/* number of Cmd3x settings 
+struct MMDMIDICmd3x {
+    uint8 struct_vers;			/* current version = 0 */
+    uint8 pad;
+    uint16 num_of_settings;		/* number of Cmd3x settings
 					 * (currently set to 15) */
-    UBYTE *ctrlr_types;			/* controller types */                  
-    UWORD *ctrlr_numbers;		/* controller numbers */
-};                                             
+    uint8 *ctrlr_types;			/* controller types */
+    uint16 *ctrlr_numbers;		/* controller numbers */
+};
 
 
 struct MMDInstrInfo {
-    UBYTE name[40];
+    uint8 name[40];
 };
 
 struct MMD0exp {
     struct MMD0 *nextmod;		/* pointer to the next module */
     struct InstrExt *exp_smp;		/* pointer to InstrExt */
-    UWORD s_ext_entries;		/* size of InstrExt structure array */
-    UWORD s_ext_entrsz;			/* size of each InstrExt structure */
-    UBYTE *annotxt;			/* pointer to the annotation text */
-    ULONG annolen;			/* length of 'annotxt' */
+    uint16 s_ext_entries;		/* size of InstrExt structure array */
+    uint16 s_ext_entrsz;		/* size of each InstrExt structure */
+    uint8 *annotxt;			/* pointer to the annotation text */
+    uint32 annolen;			/* length of 'annotxt' */
     struct MMDInstrInfo *iinfo;		/* pointer to MMDInstrInfo */
-    UWORD i_ext_entries;		/* size of MMDInstrInfo struct array */
-    UWORD i_ext_entrsz;			/* size of each MMDInstrInfo struct */
-    ULONG jumpmask;			/* mouse pointer jump control */
-    UWORD *rgbtable;			/* screen colors */
-    UBYTE channelsplit[4];		/* channel splitting control */
+    uint16 i_ext_entries;		/* size of MMDInstrInfo struct array */
+    uint16 i_ext_entrsz;		/* size of each MMDInstrInfo struct */
+    uint32 jumpmask;			/* mouse pointer jump control */
+    uint16 *rgbtable;			/* screen colors */
+    uint8 channelsplit[4];		/* channel splitting control */
     struct NotationInfo *n_info;	/* info for the notation editor */
-    UBYTE *songname;			/* song name of the current song */
-    ULONG songnamelen;			/* song name length */
+    uint8 *songname;			/* song name of the current song */
+    uint32 songnamelen;			/* song name length */
     struct MMDDumpData *dumps;		/* MIDI dump data */
     struct MMDInfo *mmdinfo;		/* more information about the song */
     struct MMDARexx *mmdrexx;		/* embedded ARexx commands */
     struct MMDMIDICmd3x *mmdcmd3x;	/* settings for command 3x */
-    ULONG reserved2[3];			/* future expansion fields */
-    ULONG tag_end;
+    uint32 reserved2[3];			/* future expansion fields */
+    uint32 tag_end;
 };
 
 
 struct NotationInfo {
-    UBYTE n_of_sharps;			/* number of sharps or flats */
+    uint8 n_of_sharps;			/* number of sharps or flats */
 #define NFLG_FLAT   1
 #define NFLG_3_4    2
-    UBYTE flags;
-    WORD trksel[5];			/* number of the selected track */
-    UBYTE trkshow[16];			/* tracks shown */
-    UBYTE trkghost[16];			/* tracks ghosted */
-    BYTE notetr[63];			/* note transpose for each instrument */
-    UBYTE pad;
+    uint8 flags;
+    int16 trksel[5];			/* number of the selected track */
+    uint8 trkshow[16];			/* tracks shown */
+    uint8 trkghost[16];			/* tracks ghosted */
+    int8 notetr[63];			/* note transpose for each instrument */
+    uint8 pad;
 };
 
 
 struct MMDDumpData {
-    UWORD numdumps;
-    UWORD reserved[3];
+    uint16 numdumps;
+    uint16 reserved[3];
 };
 
 
 struct MMDDump {
-    ULONG length;			/* length of the MIDI message dump */
-    UBYTE *data;			/* pointer to MIDI dump data */
-    UWORD ext_len;			/* MMDDump struct extension length */
+    uint32 length;			/* length of the MIDI message dump */
+    uint8 *data;			/* pointer to MIDI dump data */
+    uint16 ext_len;			/* MMDDump struct extension length */
     /* if ext_len >= 20: */
-    UBYTE name[20];			/* name of the dump */
+    uint8 name[20];			/* name of the dump */
 };
 
 extern const int mmd_num_oct[6];
