96602048d34d881627a136b9b1806e5f25e6e99c
AliceLR <petrifiedrowan@gmail.com>
2023-05-27 15:19:55 -0600

    Fix yet another out-of-bounds write in theplayer.c

--- test-dev/test_fuzzer_prowizard_p50_127_patterns.c
+++ test-dev/test_fuzzer_prowizard_p50_127_patterns.c
@@ -9,8 +9,13 @@ TEST(test_fuzzer_prowizard_p50_127_patterns)
 	opaque = xmp_create_context();
 
 	/* Pattern loader jank caused this valid module to write well out of
-	 * bounds of the pattern array. */
+	 * bounds of the pattern array. (Caused by runs.) */
 	ret = xmp_load_module(opaque, "data/f/prowizard_p50_127_patterns");
+	fail_unless(ret == 0, "module load");
+
+	/* Pattern loader jank caused this valid module to write well out of
+	 * bounds of the pattern array. (Caused by backreferences.)*/
+	ret = xmp_load_module(opaque, "data/f/prowizard_p50_127_patterns2");
 	fail_unless(ret == 0, "module load");
 
 	xmp_free_context(opaque);
