d7c2a6ef21ccea3e4e69ba5e882ba4a3172c44a5
Alice R <petrifiedrowan@gmail.com>
2023-10-24 01:28:20 -0600

    Faster IT loading by buffering pattern, sample, and comment reads. (#694)
    The IT format stores 16-bit lengths for patterns and compressed sample
    blocks, which allows both to be loaded into a buffer to be read from
    memory. This patch adds a 64k temporary buffer to the IT loader to
    reduce load times for this format.
    
    The average improvement in my loose measurements is about 45.5%
    (measured by loading every file in the Modland IT directory once with
    xmpchk: Windows 10 x86_64, all files buffered by the kernel, all files
    read into memory and loaded with `xmp_load_module_from_memory`).
    This is going to vary based on the structure of any given file.
    
    |       | master (16fc5c89) | Patch      |
    |-------|-------------------|------------|
    | Run 1 | 3m34.269s         | 1m56.595s  |
    | Run 2 | 3m34.537s         | 1m56.572s  |
    | Run 3 | 3m34.021s         | 1m57.067s  |
    | Avg.  | 3m34.276s         | 1m56.745s  |
    
    Further testing in Fedora and some additional testing by ds-sloth on
    the 3DS verifies these performance improvements.

--- src/loaders/it.h
+++ src/loaders/it.h
@@ -190,7 +190,9 @@ struct it_sample_header {
 	uint8 vit;		/* Vibrato waveform */
 };
 
-int itsex_decompress8(HIO_HANDLE *src, uint8 *dst, int len, int it215);
-int itsex_decompress16(HIO_HANDLE *src, int16 *dst, int len, int it215);
+int itsex_decompress8(HIO_HANDLE *src, uint8 *dst, int len,
+		      uint8 *tmp, int tmplen, int it215);
+int itsex_decompress16(HIO_HANDLE *src, int16 *dst, int len,
+		       uint8 *tmp, int tmplen, int it215);
 
 #endif /* LIBXMP_LOADERS_IT_H */
--- test-dev/test_fuzzer_it_invalid_compressed.c
+++ test-dev/test_fuzzer_it_invalid_compressed.c
@@ -19,6 +19,16 @@ TEST(test_fuzzer_it_invalid_compressed)
 	ret = xmp_load_module(opaque, "data/f/load_it_invalid_compressed2.it");
 	fail_unless(ret == 0, "module load");
 
+	/* This input sets the word length to 32, which can cause invalid shift
+	 * exponents if not carefully implemented. */
+	ret = xmp_load_module(opaque, "data/f/load_it_invalid_compressed3.it");
+	fail_unless(ret == 0, "module load");
+
+	/* This input sets the word length to 31, which can cause signed int
+	 * overflows if not carefully implemented. */
+	ret = xmp_load_module(opaque, "data/f/load_it_invalid_compressed4.it");
+	fail_unless(ret == 0, "module load");
+
 	xmp_free_context(opaque);
 }
 END_TEST
