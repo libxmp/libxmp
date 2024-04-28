4d77243f5e163ab40eb7dad2e5a09aaae4672f1f
Ozkan Sezer <sezeroz@gmail.com>
2024-04-28 11:50:32 +0300

    fix Android.mk after PR/669 lhasa patch.

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,10 +6,12 @@ include $(LOCAL_PATH)/src/Makefile
 include $(LOCAL_PATH)/src/loaders/Makefile
 include $(LOCAL_PATH)/src/loaders/prowizard/Makefile
 include $(LOCAL_PATH)/src/depackers/Makefile
+include $(LOCAL_PATH)/src/depackers/lhasa/Makefile
 
 SRC_SOURCES	:= $(addprefix src/,$(SRC_OBJS))
 LOADERS_SOURCES := $(addprefix src/loaders/,$(LOADERS_OBJS))
 PROWIZ_SOURCES	:= $(addprefix src/loaders/prowizard/,$(PROWIZ_OBJS))
+LHASA_SOURCES := $(addprefix src/depackers/lhasa/,$(LHASA_OBJS))
 DEPACKERS_SOURCES := $(addprefix src/depackers/,$(DEPACKERS_OBJS))
 
 LOCAL_MODULE    := xmp
@@ -17,6 +19,7 @@ LOCAL_CFLAGS	:= -O3 -DHAVE_MKSTEMP -DHAVE_FNMATCH -DHAVE_POWF -I$(LOCAL_PATH)/in
 LOCAL_SRC_FILES := $(SRC_SOURCES:.o=.c) \
 		   $(LOADERS_SOURCES:.o=.c) \
 		   $(PROWIZ_SOURCES:.o=.c) \
+		   $(LHASA_SOURCES:.o=.c) \
 		   $(DEPACKERS_SOURCES:.o=.c)
 
 include $(BUILD_STATIC_LIBRARY)
