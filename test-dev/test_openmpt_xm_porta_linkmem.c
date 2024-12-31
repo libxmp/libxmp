1fa512da2b865b9e5e95ed45f32d5c6aaddb7f00
Alice R. <petrifiedrowan@gmail.com>
2024-12-31 04:05:01 -0700

    Add XM extra fine portamento memory, unlink XM 1xx/2xx memory. (#817)
    Fixes the new version of OpenMPT Porta-LinkMem.xm with the following
    changes:
    
    * Added XM X1x and X2x extra fine portamento memory. Each effect
      has its own memory. I also added defines for the upper nibbles
      of these effects and hopefully reduced the amount of jank re: how
      `QUIRK_FINEFX` extra fine portamento enters the XM effect.
    * Unlinked 1xx and 2xx portamento up/down memory for XMs with the
      quirk `QUIRK_FT2BUGS` enabled. This may need its own quirk
      eventually.
    
    Some notes around this:
    
    * The 1xx/2xx effect handlers had some *very* old (pre-version
      control) cases to invert the portamento direction when `fxp` == 0
      that don't make any sense and broke the test module. These cases
      predate most other effects that can set `xc->freq.slide`. This
      suggests they were intended to implement shared effect memory in
      in non-XM formats (where 102 then 200 will result in the
      second effect being effectively 202). This is not necessary
      because libxmp supports shared effect memory, so I removed it.
    * The 1xx/2xx effect handlers were setting `PITCHBEND` unconditionally
      (presumably due to the aforementioned second case). This is not
      correct; 100 and 200 with no memory should not do this. This caused
      the test module to break as well.

---
+++ test-dev/test_openmpt_xm_porta_linkmem.c
@@ -0,0 +1,15 @@
+#include "test.h"
+
+/*
+   1xx, 2xx, E1x, E2x, X1x and X2x memory should not be
+   shared. Both channels should sound identical if effect memory is
+   applied correctly.
+*/
+
+TEST(test_openmpt_xm_porta_linkmem)
+{
+	compare_mixer_data(
+		"openmpt/xm/Porta-LinkMem.xm",
+		"openmpt/xm/Porta-LinkMem.data");
+}
+END_TEST
