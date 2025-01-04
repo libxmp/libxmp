70d77364ad6e7d7bca93461bb4cbc62c88d3c2e3
Ozkan Sezer <sezero@users.noreply.github.com>
2025-01-04 09:21:26 +0300

    jni/Android.mk: add missing HAVE_DIRENT definition. (#828)

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,6 @@ LOCAL_PATH	:= $(call my-dir)/..
 include $(CLEAR_VARS)
 
 # full library:
-
 include $(LOCAL_PATH)/src/Makefile
 include $(LOCAL_PATH)/src/loaders/Makefile
 include $(LOCAL_PATH)/src/loaders/prowizard/Makefile
@@ -17,7 +16,8 @@ LHASA_SOURCES := $(addprefix src/depackers/lhasa/,$(LHASA_OBJS))
 DEPACKERS_SOURCES := $(addprefix src/depackers/,$(DEPACKERS_OBJS))
 
 LOCAL_MODULE    := xmp
-LOCAL_CFLAGS	:= -O3 -DHAVE_MKSTEMP -DHAVE_FNMATCH -DHAVE_POWF -I$(LOCAL_PATH)/include
+LOCAL_CFLAGS	:= -O3 -DHAVE_MKSTEMP -DHAVE_FNMATCH -DHAVE_DIRENT -DHAVE_POWF \
+		   -I$(LOCAL_PATH)/include
 LOCAL_SRC_FILES := $(SRC_SOURCES:.o=.c) \
 		   $(LOADERS_SOURCES:.o=.c) \
 		   $(PROWIZ_SOURCES:.o=.c) \
@@ -26,14 +26,15 @@ LOCAL_SRC_FILES := $(SRC_SOURCES:.o=.c) \
 
 include $(BUILD_STATIC_LIBRARY)
 
-
 # lite library:
 include $(CLEAR_VARS)
 
 include $(LOCAL_PATH)/src/lite/Makefile
 LITE_SOURCES	:= $(addprefix src/lite/,$(LITE))
 LOCAL_MODULE    := xmp-lite
-LOCAL_CFLAGS	:= -O3 -DLIBXMP_CORE_PLAYER -DHAVE_MKSTEMP -DHAVE_FNMATCH -DHAVE_POWF -I$(LOCAL_PATH)/include
+LOCAL_CFLAGS	:= -O3 -DHAVE_MKSTEMP -DHAVE_FNMATCH -DHAVE_DIRENT -DHAVE_POWF \
+		   -DLIBXMP_CORE_PLAYER \
+		   -I$(LOCAL_PATH)/include
 LOCAL_SRC_FILES := $(LITE_SOURCES:.o=.c)
 
 include $(BUILD_STATIC_LIBRARY)
