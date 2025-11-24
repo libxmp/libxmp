cd29c186491148d9b62237825dd5ddad626e4810
Ozkan Sezer <sezeroz@gmail.com>
2025-11-24 17:56:10 +0300

    avoid -Wmissing-prototypes for mkstemp():
    	src/mkstemp.c:68: warning: no previous prototype for 'mkstemp'
    
    also rename mkstemp to libxmp_mkstemp, if not available at configuration
    time.

--- src/tempfile.h
+++ src/tempfile.h
@@ -1,5 +1,5 @@
-#ifndef XMP_PLATFORM_H
-#define XMP_PLATFORM_H
+#ifndef LIBXMP_TEMPFILE_H
+#define LIBXMP_TEMPFILE_H
 
 #include "common.h"
 
@@ -7,6 +7,11 @@ LIBXMP_BEGIN_DECLS
 
 FILE *make_temp_file(char **);
 void unlink_temp_file(char *);
+
+#ifndef HAVE_MKSTEMP
+int libxmp_mkstemp(char *);
+#define mkstemp libxmp_mkstemp
+#endif
 
 LIBXMP_END_DECLS
 
