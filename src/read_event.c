libxmp/libxmp

commit fcad98f8b84af424b639ad61788b633059bc4309

Simplify read_event_mod sample default volume handling. (#909)
Sample default volume is implemented in a strange way in
all of the libxmp_read_event implementations. This needs to
be cleaned up, as it's a significant part of why these
functions are a headache.

This patch tidies up the handling for read_event_mod ONLY.

Summary of old behavior:
* new_ins_vol was set for any e->ins present, regardless of
  validity.
* Instrument volume was applied based on the subinstrument
  from a valid e->ins ONLY in cases of toneporta (and !xc->split),
  which unsets new_ins_vol.
* new_ins_vol was unset in the case of e->note == XMP_KEY_OFF.
* There was a fully redundant clearing of new_ins_vol in cases
  of no toneporta and no valid subinstrument for the key.
  This case was already filtered by the early return.
* The event volume is then applied to xc->volume and the effects
  are run. If any of these change xc->volume, they will SET(NEW_VOL).
* The early return prevents instrument volume from being applied
  when there is no valid subinstrument.
* In backwards fashion, FINALLY the instrument volume is applied
  if e->ins && sub && new_ins_vol && !TEST(NEW_VOL) && !xc->split.
  Note the "sub" value here might not be from e->ins in cases
  of toneporta, hence the earlier special case for toneporta.

New behavior:
* xc->volume is now set directly for a valid subinstrument of
  e->ins if e->note != XMP_KEY_OFF and !xc->split. This is
  performed before handling e->volume and effects, thus
  they will naturally overwrite this value when setting volume.

The new behavior should be 100% equivalent and closer to how
these old player routines actually behave.

src/read_event.c
@@ -244,13 +244,11 @@ static int read_event_mod(struct context_data *ctx, struct xmp_event *e, int chn
 	int new_swap_ins = 0;
 	int is_toneporta;
 	int is_retrig;
-	int use_ins_vol;
 
 	xc->flags = 0;
 	note = -1;
 	is_toneporta = 0;
 	is_retrig = 0;
-	use_ins_vol = 0;
 
 	if (IS_TONEPORTA(e->fxt) || IS_TONEPORTA(e->f2t)) {
 		is_toneporta = 1;
@@ -263,7 +261,6 @@ static int read_event_mod(struct context_data *ctx, struct xmp_event *e, int chn
 
 	if (e->ins) {
 		int ins = e->ins - 1;
-		use_ins_vol = 1;
 		SET(NEW_INS);
 		xc->fadeout = 0x10000;	/* for painlace.mod pat 0 ch 3 echo */
 		xc->per_flags = 0;
@@ -283,20 +280,16 @@ static int read_event_mod(struct context_data *ctx, struct xmp_event *e, int chn
 					xc->finetune = sub->fin;
 					xc->ins = ins;
 				}
+
+				/* Dennis Lindroos: instrument volume
+				 * is not used on split channels
+				 */
+				if (!xc->split && e->note != XMP_KEY_OFF) {
+					xc->volume = sub->vol;
+				}
 			}
 
-			if (is_toneporta) {
-				/* Get new instrument volume */
-				if (sub != NULL) {
-					/* Dennis Lindroos: instrument volume
-					 * is not used on split channels
-					 */
-					if (!xc->split) {
-						xc->volume = sub->vol;
-					}
-					use_ins_vol = 0;
-				}
-			} else {
+			if (!is_toneporta) {
 				xc->ins = ins;
 				xc->ins_fade = mod->xxi[ins].rls;
 			}
@@ -329,7 +322,6 @@ static int read_event_mod(struct context_data *ctx, struct xmp_event *e, int chn
 
 		if (e->note == XMP_KEY_OFF) {
 			SET_NOTE(NOTE_RELEASE);
-			use_ins_vol = 0;
 		} else if (!is_toneporta && IS_VALID_NOTE(e->note - 1)) {
 			xc->key = e->note - 1;
 			RESET_NOTE(NOTE_END);
@@ -354,7 +346,6 @@ static int read_event_mod(struct context_data *ctx, struct xmp_event *e, int chn
 				}
 			} else {
 				xc->flags = 0;
-				use_ins_vol = 0;
 				note = xc->key;
 			}
 		}
@@ -378,6 +369,8 @@ static int read_event_mod(struct context_data *ctx, struct xmp_event *e, int chn
 		xc->smp = sub->sid;
 	}
 
+	/* sub is now the currently playing subinstrument, which may not be
+	 * related to e->ins if there is active toneporta! */
 	sub = get_subinstrument(ctx, xc->ins, xc->key);
 
 	set_effect_defaults(ctx, note, sub, xc, is_toneporta);
@@ -424,10 +417,6 @@ static int read_event_mod(struct context_data *ctx, struct xmp_event *e, int chn
 			xc->offset.val += xc->offset.val2;
 		}
 		RESET(OFFSET);
-	}
-
-	if (use_ins_vol && !TEST(NEW_VOL) && !xc->split) {
-		xc->volume = sub->vol;
 	}
 
 	return 0;

commit e62f358fd5139843f1f9b26ecd35648be75622d1

Fix read_event_mod/read_event_st3 multisample defaults.
In read_event_mod and read_event_st3, xc->key is set if e->note is
a valid note and there is no toneporta. The event is read-only and
the toneporta state never changes, therefore xc->key can be set
before handling the instrument. This allows the subinstrument
lookups to use the correct key to get default volume/panning.
Previously, default volume was set at the end of the functions
using the correct xc->key via a convoluted chain of checks.

Fixes minor regressions from #909 and #911. The only multisample
format currently using either of these players is Imago Orpheus IMF,
which doesn't closely match any of the current read event functions.

src/read_event.c
@@ -286,6 +286,9 @@ static int read_event_mod(struct context_data *ctx, const struct xmp_event *e, i
 
 	/* Check instrument */
 
+	if (IS_VALID_NOTE(e->note - 1) && !is_toneporta) {
+		xc->key = e->note - 1;
+	}
 	if (e->ins) {
 		int ins = e->ins - 1;
 		SET(NEW_INS);
@@ -297,7 +300,7 @@ static int read_event_mod(struct context_data *ctx, const struct xmp_event *e, i
 		RESET_NOTE(NOTE_RELEASE|NOTE_FADEOUT);
 
 		if (IS_VALID_INSTRUMENT(ins)) {
-			sub = get_subinstrument(ctx, ins, e->note - 1);
+			sub = get_subinstrument(ctx, ins, xc->key);
 
 			if (sub != NULL) {
 				new_swap_ins = 1;
@@ -357,7 +360,6 @@ static int read_event_mod(struct context_data *ctx, const struct xmp_event *e, i
 		if (e->note == XMP_KEY_OFF) {
 			SET_NOTE(NOTE_RELEASE);
 		} else if (!is_toneporta && IS_VALID_NOTE(e->note - 1)) {
-			xc->key = e->note - 1;
 			RESET_NOTE(NOTE_END);
 
 			sub = get_subinstrument(ctx, xc->ins, xc->key);
@@ -744,6 +746,9 @@ static int read_event_st3(struct context_data *ctx, const struct xmp_event *e, i
 
 	/* Check instrument */
 
+	if (IS_VALID_NOTE(e->note - 1) && !is_toneporta) {
+		xc->key = e->note - 1;
+	}
 	if (e->ins) {
 		int ins = e->ins - 1;
 		SET(NEW_INS);
@@ -762,7 +767,7 @@ static int read_event_st3(struct context_data *ctx, const struct xmp_event *e, i
 			}
 
 			/* Get new instrument volume */
-			sub = get_subinstrument(ctx, ins, e->note - 1);
+			sub = get_subinstrument(ctx, ins, xc->key);
 			if (sub != NULL && e->note != XMP_KEY_OFF) {
 				set_channel_volume(xc, sub->vol);
 				set_channel_pan(xc, sub->pan);
@@ -788,7 +793,6 @@ static int read_event_st3(struct context_data *ctx, const struct xmp_event *e, i
 				xc->offset.val = 0;
 			}
 		} else if (IS_VALID_NOTE(e->note - 1)) {
-			xc->key = e->note - 1;
 			RESET_NOTE(NOTE_END);
 
 			sub = get_subinstrument(ctx, xc->ins, xc->key);

test-dev/test_no_note_same_ins_mod.c
@@ -107,7 +107,7 @@ TEST(test_no_note_same_ins_mod)
 	/* Row 5 */
 	xmp_play_frame(opaque);
 	check_on(xc, vi, KEY_B5, INS_0,
-		 -1 /* FIXME: INS_0_SUB_1_VOL */, -1 /* FIXME: INS_0_SUB_1_PAN */, INS_0_FADE, "row 5");
+		 INS_0_SUB_1_VOL, INS_0_SUB_1_PAN, INS_0_FADE, "row 5");
 
 	xmp_play_frame(opaque);
 

test-dev/test_no_note_same_ins_st3.c
@@ -108,7 +108,7 @@ TEST(test_no_note_same_ins_st3)
 	/* Row 5 */
 	xmp_play_frame(opaque);
 	check_on(xc, vi, KEY_B5, INS_0,
-		 -1 /* FIXME: INS_0_SUB_1_VOL */, -1 /* FIXME: INS_0_SUB_1_PAN */, INS_0_FADE, "row 5");
+		 INS_0_SUB_1_VOL, INS_0_SUB_1_PAN, INS_0_FADE, "row 5");
 
 	xmp_play_frame(opaque);
 