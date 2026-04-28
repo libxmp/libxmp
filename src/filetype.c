ea89f0b3dd5336edbc08fd43610e4a5fdc184e67
Ozkan Sezer <sezeroz@gmail.com>
2026-04-28 08:56:50 +0300

    add OS/2 EMX makefiles for KLIBC environment.
    Build tested under linux using cross-os2emx toolchain at
    https://github.com/komh/cross-os2emx

--- src/filetype.c
+++ src/filetype.c
@@ -1,5 +1,5 @@
 /* Extended Module Player
- * Copyright (C) 1996-2022 Claudio Matsuoka and Hipolito Carraro Jr
+ * Copyright (C) 1996-2026 Claudio Matsuoka and Hipolito Carraro Jr
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -48,7 +48,7 @@ int libxmp_get_filetype (const char *path)
 int libxmp_get_filetype (const char *path)
 {
 	FILESTATUS3 fs;
-	if (DosQueryPathInfo(path, FIL_STANDARD, &fs, sizeof(fs)) != 0) {
+	if (DosQueryPathInfo((PCSZ)path, FIL_STANDARD, &fs, sizeof(fs)) != 0) {
 	    errno = ENOENT;
 	    return XMP_FILETYPE_NONE;
 	}
