fe6579c60bc59e32087653ff43e2fd53c613a7ba
AliceLR <petrifiedrowan@gmail.com>
2026-06-21 20:23:55 -0600

    Rewrite smix WAV loader and fix smix sample load/free bugs.
    * xmp_smix_load_sample: add support for loading stereo samples.
    * xmp_smix_load_sample: fix signedness of loaded 8-bit samples.
    * xmp_smix_load_sample: slightly better WAV format integrity checks.
    * xmp_smix_load_sample: always call xmp_smix_release_sample on the
      destination instrument if the requested file successfully opens.
      Previously, this would leak old sample/subinstrument data.
    * xmp_smix_release_sample: fix crashes if the released sample is
      currently being played by a mixer voice.

--- test-dev/test_api_smix_load_sample.c
+++ test-dev/test_api_smix_load_sample.c
@@ -1,12 +1,41 @@
 #include <errno.h>
 #include "test.h"
 
+#define MD5_8BIT_MONO		"3ef6168240590762450a7cd65deaebfa"
+#define MD5_16BIT_MONO		"3d16ad99b4a80ea35719344e4bb5da17"
+#define MD5_16BIT_STEREO	"11d9e7f24987932818709ea9bf1c0a57"
+
+static inline void smix_compare_md5(const struct xmp_sample *xxs,
+				    const char *digest, const char *desc)
+{
+	MD5_CTX ctx;
+	unsigned char d[16];
+	size_t len = xxs->len;
+	int ret;
+
+	if (xxs->flg & XMP_SAMPLE_16BIT)
+		len <<= 1;
+	if (xxs->flg & XMP_SAMPLE_STEREO)
+		len <<= 1;
+
+	MD5Init(&ctx);
+	MD5Update(&ctx, xxs->data, len);
+	MD5Final(d, &ctx);
+
+	ret = compare_md5(d, digest);
+	fail_unless(ret == 0, desc);
+}
+
 TEST(test_api_smix_load_sample)
 {
 	xmp_context opaque;
+	struct context_data *ctx;
+	struct smix_data *smix;
 	int ret;
 
 	opaque = xmp_create_context();
+	ctx = (struct context_data *)opaque;
+	smix = &ctx->smix;
 
 	ret = xmp_load_module(opaque, "data/mod.loving_is_easy.pp");
 	fail_unless(ret == 0, "load module");
@@ -33,12 +62,19 @@ TEST(test_api_smix_load_sample)
         ret = xmp_smix_load_sample(opaque, 1, "data/mod.loving_is_easy.pp");
 	fail_unless(ret == -XMP_ERROR_FORMAT, "invalid format");
 
-	/* try to load stereo sample */
-        ret = xmp_smix_load_sample(opaque, 1, "data/send.wav");
-	fail_unless(ret == -XMP_ERROR_FORMAT, "invalid format");
-
 	ret = xmp_smix_load_sample(opaque, 0, "data/blip.wav");
 	fail_unless(ret == 0, "load sample");
+	smix_compare_md5(&smix->xxs[0], MD5_16BIT_MONO, "data/blip.wav MD5");
+
+	/* Double-load should not leak */
+	ret = xmp_smix_load_sample(opaque, 0, "data/blip8.wav");
+	fail_unless(ret == 0, "load sample");
+	smix_compare_md5(&smix->xxs[0], MD5_8BIT_MONO, "data/blip8.wav MD5");
+
+	/* try to load stereo sample */
+	ret = xmp_smix_load_sample(opaque, 1, "data/send.wav");
+	fail_unless(ret == 0, "load sample (stereo)");
+	smix_compare_md5(&smix->xxs[1], MD5_16BIT_STEREO, "data/send.wav MD5");
 
 	ret = xmp_smix_release_sample(opaque, 0);
 	fail_unless(ret == 0, "release sample");
